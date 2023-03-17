2c4bf4d54328dc19bd5136ed28cdc042c4fa3c12
ycj <108540613@qq.com>
2023-03-17 22:28:40 +0800

    UDF布局

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,30 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IconsProviderModule } from './icons-provider.module';
+import { IconDefinition } from '@ant-design/icons-angular';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzInputModule } from 'ng-zorro-antd/input';
+
+import * as AllIcons from '@ant-design/icons-angular/icons';
+
 import { StreamComponent } from './pages/stream/stream.component';
 import { BatchComponent } from './pages/batch/batch.component';
 import { DataSourceComponent } from './pages/datasource/datasource.component';
 import { UdfComponent } from './pages/udf/udf.component';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+
+const antDesignIcons = AllIcons as {
+  [key: string]: IconDefinition;
+};
+
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key]);
 
 registerLocaleData(zh);
 
@@ -27,7 +44,8 @@ registerLocaleData(zh);
     StreamComponent,
     BatchComponent,
     DataSourceComponent,
-    UdfComponent
+    UdfComponent,
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -38,7 +56,14 @@ registerLocaleData(zh);
     IconsProviderModule,
     NzLayoutModule,
     NzMenuModule,
-    NzIconModule,
+    NzSpaceModule,
+    NzPageHeaderModule,
+    NzButtonModule,
+    NzDividerModule,
+    NzTableModule,
+    FormsModule,
+    NzInputModule,
+    NzIconModule.forRoot(icons),
   ],
   providers: [
     { provide: NZ_I18N, useValue: zh_CN }
--- src/app/pages/udf/udf.component.ts
+++ src/app/pages/udf/udf.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+
+interface Person {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+}
+
 @Component({
   selector: 'app-udf',
   templateUrl: './udf.component.html',
   styleUrls: ['./udf.component.less']
 })
 export class UdfComponent {
-
+  listOfData: Person[] = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park'
+    }
+  ];
 }
